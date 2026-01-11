3fc05ce9c72075184345557087b09f68ebb88515
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-11 13:38:41 +0530

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,42 +1,21 @@
-// profile.js
-
-const token = localStorage.getItem("token");
-
-if (!token) {
-  window.location.href = "index.html";
-}
-
 async function loadProfile() {
-  try {
-    const res = await fetch(API + "/profile", {
-      headers: {
-        Authorization: token
-      }
-    });
-
-    const data = await res.json();
-
-    if (!res.ok) {
-      alert("Session expired");
-      logout();
-      return;
+  const res = await fetch(API + "/profile", {
+    headers: {
+      Authorization: localStorage.getItem("token")
     }
+  });
 
-    document.getElementById("mobile").innerText = data.mobile;
-    document.getElementById("wallet").innerText = data.wallet;
-    document.getElementById("wagered").innerText = data.totalWagered;
-  } catch {
-    alert("Server error");
+  const data = await res.json();
+
+  if (!res.ok) {
+    alert("Session expired");
+    logout();
+    return;
   }
-}
 
-function goWithdraw() {
-  window.location.href = "withdraw.html";
-}
-
-function logout() {
-  localStorage.removeItem("token");
-  window.location.href = "index.html";
+  document.getElementById("mobile").innerText = data.mobile;
+  document.getElementById("wallet").innerText = data.wallet;
+  document.getElementById("wagered").innerText = data.totalWagered;
 }
 
 loadProfile();
