e71b3fffca0100999a3e26f88192f68007abbff9
ohmusic123-del <ohmusic.123@gmail.com>
2026-02-14 23:34:54 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// API Configuration
+const API = 'https://color-game-backend1.onrender.com';
+
+// Export for use in other files
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { API };
+}
